2dadadd41f1b38fcf6bb4e900460d9d3cb0b6eed
Tarciso Torres <tarciso.torres@ifactory.com.br>
2018-04-28 22:43:40 -0300

    First Commit

---
+++ src/main/java/com/schoolofnet/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,12 @@
+package com.schoolofnet.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+}
